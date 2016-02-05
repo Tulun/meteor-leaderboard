ddd065bdea22fca201107eaf5f3d5a399ed0650d
Tulun <jaykiraly@gmail.com>
2016-02-05 12:39:46 -0800

    Score listed by highest to lowest, alphabetical.

--- leaderboard.js
+++ leaderboard.js
@@ -5,7 +5,7 @@ console.log('Hello, world!');
 if(Meteor.isClient) {
   Template.leaderboard.helpers({
     'player': function(){
-      return PlayersList.find()
+      return PlayersList.find({}, {sort: {score: -1, name: 1} })
     },
 
     'selectedClass': function() {
