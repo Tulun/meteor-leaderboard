a1986b1501808d27f0b2f84be9a603bc41a20cc4
Tulun <jaykiraly@gmail.com>
2016-02-05 12:00:38 -0800

    Added first helper function.

--- leaderboard.js
+++ leaderboard.js
@@ -1,2 +1,16 @@
 PlayersList = new Mongo.Collection('players');
 
+console.log('Hello, world!');
+
+if(Meteor.isClient) {
+  console.log('Hello client.');
+  Template.leaderboard.helpers({
+    'player': function(){
+      return 'Some text'
+    }
+  });
+};
+
+if(Meteor.isServer) {
+  console.log('Hello server.')
+};
